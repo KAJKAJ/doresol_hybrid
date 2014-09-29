224dd7b6a42c1786b4e7f6571ba6bca4036986f8
SunJang <realjangsun@gmail.com>
2014-09-29 15:32:59 +0900

    add famo.us and firebase

--- app/js/config.js
+++ app/js/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
     // Init module configuration options
     var applicationModuleName = 'doresolApp';
-    var applicationModuleVendorDependencies = ['ngResource', 'ngCookies', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils'];
+    var applicationModuleVendorDependencies = ['famous.angular','ngResource', 'ngCookies', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils','firebase'];
 
     // Add a new vertical module
     var registerModule = function(moduleName) {
