28ee1bb640764cbd47be150171841efc1934c85d
SunJang <realjangsun@gmail.com>
2014-09-29 11:38:24 +0900

    first commit

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,54 @@
+NG_DOCS={
+  "sections": {
+    "api": "App Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "core",
+      "shortName": "core",
+      "type": "overview",
+      "moduleName": "core",
+      "shortDescription": "The angular services, filters, directives, filters within the core module are accessible throughout the angular app like any other provider within the app, but these providers do not necessarily belong to any particular module, hence their placement would be here.",
+      "keywords": "accessible angular api app belong core directives filters module overview placement provider providers services"
+    },
+    {
+      "section": "api",
+      "id": "core.config",
+      "shortName": "core.config",
+      "type": "object",
+      "moduleName": "core",
+      "shortDescription": "Defines the routes and other config within the core module",
+      "keywords": "$stateprovider $urlrouterprovider api config core defines module ng object routes"
+    },
+    {
+      "section": "api",
+      "id": "core.Controllers.HomeController",
+      "shortName": "core.Controllers.HomeController",
+      "type": "object",
+      "moduleName": "core.Controllers",
+      "shortDescription": "Home controller",
+      "keywords": "$scope api controller controllers core homecontroller ng object"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "jquery.js",
+    "bootstrap.js",
+    "angular.js",
+    "angular-resource.js",
+    "angular-mocks.js",
+    "angular-cookies.js",
+    "angular-sanitize.js",
+    "angular-animate.js",
+    "angular-touch.js",
+    "ui-bootstrap.js",
+    "ui-utils.js",
+    "angular-ui-router.js"
+  ]
+};
