4be0ced46fcf5fa6e6ff7eee9bb71ea7e469b231
SunJang <realjangsun@gmail.com>
2014-11-18 18:30:02 +0900

    merge story

--- app/modules/core/controllers/story.js
+++ app/modules/core/controllers/story.js
@@ -20,39 +20,56 @@ angular
   $scope.mode = 'overview';
   $scope.storiesArray = MyStory.getStoriesArray();
   $scope.storiesObject = MyStory.getStoriesObject();
-  $scope.storiesCnt = MyStory.getStoriesCnt();
 
   $scope.role = Memorial.getRole();
 
-  $scope.boxSize = 155;
-  $scope.windowWidth = window.innerWidth;
 
-  $(window).resize(function() {
-    $scope.windowWidth = window.innerWidth;
-    $scope.$apply(function() {
-       //do something to update current scope based on the new innerWidth and let angular update the view.
-       calculateGrid();
-    });
-  });
+  $scope.tempArray = [];
 
-  var calculateGrid = function(){
-    var cols = 0;
-    var rows = 0;
-   
-    cols = Math.floor($scope.windowWidth/$scope.boxSize);
-    rows = Math.ceil($scope.storiesArray.length/cols);
-    
-    $scope.gridHeight = $scope.boxSize*rows;
-    $scope.gridLayoutOptions = {
-      dimensions: [cols,rows], // specifies number of columns and rows
-    };
+  for(var i=0;i<20;i++){
+    $scope.tempArray[i] = {
+      url:'http://doresol.net:8000/tmp/facebook10152657766018959-1416293149194-5m0ri9ts9jcyp-2010-11-1517.34.37.jpg'
+    }
   }
 
-  $scope.$watch( function(){ return MyStory.getStoriesCnt();}, function(newValue){
-    if($scope.storiesArray.length > 0){
-      calculateGrid();
-    }
-  });
+  $scope.sequentialOptions = {
+          direction: 1, // vertical = 1 (default), horizontal = 0
+        };
+ 
+  // console.log($scope.storiesArray);
+  // console.log($scope.storiesObject);
+
+  // $scope.storiesCnt = MyStory.getStoriesCnt();
+
+  // $scope.boxSize = 155;
+  // $scope.windowWidth = window.innerWidth;
+
+  // $(window).resize(function() {
+  //   $scope.windowWidth = window.innerWidth;
+  //   $scope.$apply(function() {
+  //      //do something to update current scope based on the new innerWidth and let angular update the view.
+  //      calculateGrid();
+  //   });
+  // });
+
+  // var calculateGrid = function(){
+  //   var cols = 0;
+  //   var rows = 0;
+   
+  //   cols = Math.floor($scope.windowWidth/$scope.boxSize);
+  //   rows = Math.ceil($scope.storiesArray.length/cols);
+    
+  //   $scope.gridHeight = $scope.boxSize*rows;
+  //   $scope.gridLayoutOptions = {
+  //     dimensions: [cols,rows], // specifies number of columns and rows
+  //   };
+  // }
+
+  // $scope.$watch( function(){ return MyStory.getStoriesCnt();}, function(newValue){
+  //   if($scope.storiesArray.length > 0){
+  //     calculateGrid();
+  //   }
+  // });
 
   $scope.scrollContentHeight = {};
 
